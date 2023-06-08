caca5161dd273bc0a804afd473d5b0eac6dd21e3
Alex Visan <alexandruvisan1908@gmail.com>
2023-06-08 07:39:08 +0300

    add fixes

---
+++ .netlify/functions-internal/chunks/index@_@astro.1f5b2d17.mjs
@@ -0,0 +1,7 @@
+export { renderers } from '../renderers.mjs';
+import './astro.dc1d60d1.mjs';
+import 'preact';
+
+const page = () => import('./prerender.dfb40013.mjs').then(n => n.f);
+
+export { page };
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,15 +1,16 @@
 import { useState } from "preact/hooks";
+import Typewriter from "typewriter-effect";
 
 export default function Greeting({ messages }) {
-  const randomMessage = () =>
-    messages[Math.floor(Math.random() * messages.length)];
-
-  const [greeting, setGreeting] = useState(messages[0]);
-
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      <Typewriter
+        options={{
+          strings: ["Hello", "World"],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
   );
 }
