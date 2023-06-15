25151d1740665f018a944d43beb946d1377cf9b9
Alex Visan <alexandruvisan1908@gmail.com>
2023-06-15 21:08:35 +0300

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: ["./src/**/*.{astro,tsx,ts,jsx,js}"],
   theme: {
     extend: {
+      animation: {
+        wiggle: "wiggle 0.9s ease-in-out infinite",
+      },
       keyframes: {
-        wave: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(15deg)" },
+          "50%": { transform: "rotate(65deg)" },
         },
       },
     },
