2edb1b531b3b30f7097e5b48ed1732e2e48b7c7f
Alex Visan <alexandruvisan1908@gmail.com>
2023-06-16 13:10:55 +0300

    change wave animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ export default {
   theme: {
     extend: {
       animation: {
-        wiggle: "wiggle 0.9s ease-in-out infinite",
+        wiggle: "wiggle 2s ease-in-out infinite",
       },
       keyframes: {
         wiggle: {
-          "0%, 100%": { transform: "rotate(15deg)" },
-          "50%": { transform: "rotate(65deg)" },
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14.0deg)" },
+          "20%": { transform: "rotate(-8.0deg)" },
+          "30%": { transform: "rotate(14.0deg)" },
+          "40%": { transform: "rotate(-4.0deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate( 0.0deg)" },
+         "100%": { transform: "rotate( 0.0deg)" },
         },
       },
     },
