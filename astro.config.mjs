6c97749929af33dfae2e284ec1075486d6366a57
Alex Visan <alexandruvisan1908@gmail.com>
2023-06-16 11:15:03 +0300

    add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexvisan.netlify.app/',
   integrations: [preact(), tailwind()],
   output: "hybrid",
   adapter: netlify(),
