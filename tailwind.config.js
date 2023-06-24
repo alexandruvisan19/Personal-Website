dde399a977a31dd3143511785b73cea74950b374
Alex Visan <alexandruvisan1908@gmail.com>
2023-06-24 23:01:14 +0300

    changeS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ export default {
     extend: {
       animation: {
         wave: 'wave 2.5s ease-in-out infinite',
+        color: 'color 30s infinite linear',
+        changeGradient: 'gradientAnimation 5s linear infinite'
       },
       keyframes: {
         wave: {
@@ -19,11 +21,31 @@ export default {
           '60%': { transform: 'rotate( 0.0deg)' },
           '100%': { transform: 'rotate( 0.0deg)' },
         },
+         color: {
+          '0%':   { backgroundColor: '#9336B4' },
+          '20%':  { backgroundColor: '#0A6EBD' },
+          '40%':  { backgroundColor: '#1F6E8C' },
+          '60%':  { backgroundColor: '#4942E4' },
+          '80%':  { backgroundColor: '#116D6E' },
+          '100%': { backgroundColor: '#9336B4' },
+        },
+        gradientAnimation: {
+          '0%': {
+            backgroundPosition: '0% 50%'
+          },
+          '100%': {
+            backgroundPosition: '100% 50%'
+          }
+        }
       },
       fontFamily: {
         carter: ['Carter One', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
         montserratItalic: ['Montserrat Italic', 'sans-serif'],
+      },
+      backgroundImage: {
+        heroPattern:
+          "url('noise.webp')",
       },
     },
   },
